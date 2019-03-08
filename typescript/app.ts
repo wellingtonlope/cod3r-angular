e5dd7a8870e4ae58b14ed72d2b28dc1ab1095a57
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-03-08 00:10:31 -0400

    initial commit

---
+++ typescript/app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, Obi-wan Kenobi. You're my only hope!"
+console.log(message)
+
+let episode: number = 4
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+let favortiteDroid: string
+favortiteDroid = 'BB-8'
+console.log("My favorite droid is " + favortiteDroid)
---
+++ typescript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favortiteDroid;
+favortiteDroid = 'BB-8';
+console.log("My favorite droid is " + favortiteDroid);
