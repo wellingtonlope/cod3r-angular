8489a8a4ba3963d19086c42729649c4f13d6dcc5
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-03-08 10:59:32 -0400

    first application

---
+++ jedi-academy/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { Student } from './student/student.model'
+
+@Component({
+  selector: 'jad-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ]
+}
