538929712f93c632529029474710a9a0bf42748a
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-03-11 19:28:07 -0400

    refactoring to reactive forms

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -1,33 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { ROUTES } from 'app/app.routes'
+import { ROUTES } from "app/app.routes";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
+import { RestaurantsService } from "./restaurants/restaurants.service";
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
+import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
+import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderComponent } from "./order/order.component";
+import { InputComponent } from "./shared/input/input.component";
+import { RadioComponent } from "./shared/radio/radio.component";
+import { OrderItemsComponent } from "./order/order-items/order-items.component";
+import { OrderService } from "./order/order.service";
+import { DeliveryCostsComponent } from "./order/delivery-costs/delivery-costs.component";
+import { OrderSummaryComponent } from "./order-summary/order-summary.component";
+import { RatingComponent } from "./shared/rating/rating.component";
 
 @NgModule({
   declarations: [
@@ -54,9 +53,15 @@ import { RatingComponent } from './shared/rating/rating.component';
     BrowserModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [
+    RestaurantsService,
+    ShoppingCartService,
+    OrderService,
+    { provide: LOCALE_ID, useValue: "pt-BR" }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 import { RadioOption } from "app/shared/radio/radio-option.model";
 import { OrderService } from "./order.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
@@ -10,6 +16,11 @@ import { Router } from "@angular/router";
   templateUrl: "./order.component.html"
 })
 export class OrderComponent implements OnInit {
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  numberPattern = /^[0-9]*$/;
+
+  orderForm: FormGroup;
+
   delivery: number = 8;
 
   paymentOptions: RadioOption[] = [
@@ -18,9 +29,55 @@ export class OrderComponent implements OnInit {
     { label: "Cartão Refeição", value: "REF" }
   ];
 
-  constructor(private orderService: OrderService, private router: Router) {}
+  constructor(
+    private orderService: OrderService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group(
+      {
+        name: this.formBuilder.control("", [
+          Validators.required,
+          Validators.minLength(5)
+        ]),
+        email: this.formBuilder.control("", [
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ]),
+        emailConfirmation: this.formBuilder.control("", [
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ]),
+        address: this.formBuilder.control("", [
+          Validators.required,
+          Validators.minLength(5)
+        ]),
+        number: this.formBuilder.control("", [
+          Validators.required,
+          Validators.pattern(this.numberPattern)
+        ]),
+        optionalAddress: this.formBuilder.control(""),
+        paymentOption: this.formBuilder.control("", [Validators.required])
+      },
+      { validator: OrderComponent.equalsTo }
+    );
+  }
+
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get("email");
+    const emailConfirmation = group.get("emailConfirmation");
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true };
+    }
+
+    return undefined;
+  }
 
   itemsValue(): number {
     return this.orderService.itemsValue();
@@ -47,8 +104,8 @@ export class OrderComponent implements OnInit {
       (item: CartItem) => new OrderItem(item.quantity, item.menuItem.id)
     );
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
-      this.router.navigate(['/order-summary'])
-      this.orderService.clear()
+      this.router.navigate(["/order-summary"]);
+      this.orderService.clear();
     });
   }
 }
