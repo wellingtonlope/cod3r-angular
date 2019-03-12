891d7d35f9c08adc0b2235f2235a77897cd7d33b
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-03-11 20:43:42 -0400

    add modules

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { AboutComponent } from './about/about.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
@@ -19,7 +17,7 @@ export const ROUTES: Routes = [
       {path: 'reviews', component: ReviewsComponent}
     ]
   },
-  {path: 'order', component: OrderComponent},
+  {path: 'order', loadChildren: './order/order.module#OrderModule'},
   {path: 'order-summary', component: OrderSummaryComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', loadChildren: './about/about.module#AboutModule'}
 ]
---
+++ meat-app-starter/src/app/order/order.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { SharedModule } from "app/shared/shared.module";
+import { OrderComponent } from "./order.component";
+import { OrderItemsComponent } from "./order-items/order-items.component";
+import { DeliveryCostsComponent } from "./delivery-costs/delivery-costs.component";
+
+const ROUTES: Routes = [
+  {path: '', component: OrderComponent}
+]
+
+@NgModule({
+  declarations: [OrderComponent, OrderItemsComponent, DeliveryCostsComponent],
+  imports: [SharedModule, RouterModule.forChild(ROUTES)]
+})
+export class OrderModule {}
